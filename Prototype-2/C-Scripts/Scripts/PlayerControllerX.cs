9bef4b740d39af591084c9ba338728a50656291f
Jon Curtis <81921197+MissingCurtis@users.noreply.github.com>
2022-01-19 15:35:04 -0600

    Add files via upload

---
+++ Prototype-2/C-Scripts/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    // check this
+    private bool spawned = false;
+    private float decay;
+    // Update is called once per frame
+    void Update()
+    {
+        Reset();
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !spawned)
+        {
+            decay = 1f;
+            spawned = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+        
+    }
+    private void Reset()
+    {
+        if (spawned && decay > 0)
+        {
+            decay -= Time.deltaTime;
+        }
+        if (decay < 0)
+        {
+            decay = 0;
+            spawned = false;
+        }
+    }
+
+}
